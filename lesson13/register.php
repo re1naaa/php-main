be2d6ac49afd2bb2318b27b04b405dcac3bcf7c6
re1naaa <Reinabeadini@gmail.com>
2025-09-25 17:36:11 +0200

    lesson13

---
+++ lesson13/register.php
@@ -0,0 +1,47 @@
+<?php
+include_once('config.php');
+
+if(isset($_POST['submit']))
+{
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $temPass = $_POST['password'];
+
+    $password = $temPass;
+    
+    if(empty($name))  || empty($surname) || empty($username) || empty($email) || empty($temPass)
+    {
+        echo "You nedd to fill all the fields.";
+    }else
+    {
+        $sql = "SELECT username FROM users WHERE username=:username";
+        $tempSQL = $conn->prepare($sql);
+        $tempSQL->bindParam(':username', $username);
+        $tempSQL->exetucte();
+
+        if($tempSQL->rowCount() > 0)
+        {
+            echo"Username exists!";
+            header("refresh:2; url=singup.php");
+        }else
+        {
+            $sql = "INSERT INTO users (name, surname, username, email, password) VALUES (:name, :surname, :username, :email :password)";
+            $insertSql = $conn->prepare($sql);
+
+            $insertSql->bindParam(':name', $name);
+            $insertSql->bindParam(':surname', $surname);
+            $insertSql->bindParam(':username', $username);
+            $insertSql->bindParam(':email', $email);
+            $insertSql->bindParam(':name', $password);
+
+            $insertSql->execute();
+
+            echo"Data saved successfully ...";
+            header("refresh:2; url=login.php")
+        }
+    }
+}
+
+?>
